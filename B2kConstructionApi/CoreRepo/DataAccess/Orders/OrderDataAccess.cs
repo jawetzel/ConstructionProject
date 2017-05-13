96c7d3bfd367e440f672ed539a182caa4e5e5640
JoshuaDesktop <jawetzel615@gmail.com>
2017-05-12 20:38:31 -0500

    crud for order and order request

---
+++ B2kConstructionApi/CoreRepo/DataAccess/Orders/OrderDataAccess.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreRepo.Data;
+using CoreRepo.Data.Orders;
+
+namespace CoreRepo.DataAccess.Orders
+{
+    public class OrderDataAccess
+    {
+        private readonly Context DbContext;
+        public OrderDataAccess(Context dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public List<Order> GetListOfAllOrders()
+        {
+            return DbContext.Orders.Where(order => order.IsActive).ToList();
+        }
+
+        public List<Order> GetListOfAllOrdersAppointmentNeeded()
+        {
+            return DbContext.Orders.Where(order => order.IsActive && order.IsAppointmentNeeded).ToList();
+        }
+
+        public List<Order> GetListOfAllOrdersComleted()
+        {
+            return DbContext.Orders.Where(order => order.IsActive && order.IsCompleted).ToList();
+        }
+
+        public List<Order> GetListOfAllOrdersByCustomerId(int id)
+        {
+            return DbContext.Orders.Where(order => order.IsActive && order.UserId == id).ToList();
+        }
+
+        public List<Order> GetListOfAllOrdersByCustomerName(string name)
+        {
+            return DbContext.Orders.Where(order => order.IsActive && order.User.Name.ToLower().Contains(name.ToLower())).ToList();
+        }
+
+        public Order GetListOfAllOrdersByOrderRequestId(int id)
+        {
+            try
+            {
+                return DbContext.Orders.First(order => order.IsActive && order.OrderRequestId == id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public Order GetOrderById(int id)
+        {
+            try
+            {
+                return DbContext.Orders.First(order => order.IsActive && order.Id == id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public Order UpdateOrder(Order order)
+        {
+            try
+            {
+                order.LastEditedDateTime = DateTime.UtcNow;
+                DbContext.Orders.Update(order);
+                DbContext.SaveChanges();
+                return order;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public bool DeleteOrder(Order order)
+        {
+            try
+            {
+                order.LastEditedDateTime = DateTime.UtcNow;
+                order.IsActive = false;
+                DbContext.Orders.Update(order);
+                DbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
---
+++ B2kConstructionApi/CoreRepo/DataAccess/Orders/OrderRequestDataAccess.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreRepo.Data;
+using CoreRepo.Data.Orders;
+
+namespace CoreRepo.DataAccess.Orders
+{
+    public class OrderRequestDataAccess
+    {
+        private readonly Context DbContext;
+        public OrderRequestDataAccess(Context dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public List<OrderRequest> GetListOfAllOrderRequests()
+        {
+            return DbContext.OrderRequests.Where(orderReq => orderReq.IsActive).ToList();
+        }
+
+        public List<OrderRequest> GetListOfAllOrderRequestsByName(string name)
+        {
+            return DbContext.OrderRequests.Where(orderReq => orderReq.IsActive && orderReq.Name.ToLower().Contains(name.ToLower())).ToList();
+        }
+
+        public List<OrderRequest> GetListOfAllOrderRequestsByEmail(string email)
+        {
+            return DbContext.OrderRequests.Where(orderReq => orderReq.IsActive && orderReq.Email.ToLower().Contains(email.ToLower())).ToList();
+        }
+
+        public List<OrderRequest> GetListOfAllOrderRequestsByPhone(string phone)
+        {
+            return DbContext.OrderRequests.Where(orderReq => orderReq.IsActive && orderReq.PhoneNumber.ToLower().Contains(phone.ToLower())).ToList();
+        }
+
+        public List<OrderRequest> GetListOfAllOrderRequestsByStreetAddress(string address)
+        {
+            return DbContext.OrderRequests.Where(orderReq => orderReq.IsActive && orderReq.AddressStreet.ToLower().Contains(address.ToLower())).ToList();
+        }
+
+        public List<OrderRequest> GetListOfAllOrderRequestsWhereCalled()
+        {
+            return DbContext.OrderRequests.Where(orderReq => orderReq.IsActive && orderReq.Called).ToList();
+        }
+
+        public List<OrderRequest> GetListOfAllOrderRequestsWhereNotCalled()
+        {
+            return DbContext.OrderRequests.Where(orderReq => orderReq.IsActive && !orderReq.Called).ToList();
+        }
+
+        public OrderRequest GetOrderRequestById(int id)
+        {
+            try
+            {
+                return DbContext.OrderRequests.First(orderReq => orderReq.IsActive && orderReq.Id == id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public OrderRequest UpdateOrderRequest(OrderRequest orderReq)
+        {
+            try
+            {
+                orderReq.LastEditedDateTime = DateTime.UtcNow;
+                DbContext.OrderRequests.Update(orderReq);
+                DbContext.SaveChanges();
+                return orderReq;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public bool DeleteOrderRequest(OrderRequest orderReq)
+        {
+            try
+            {
+                orderReq.LastEditedDateTime = DateTime.UtcNow;
+                orderReq.IsActive = false;
+                DbContext.OrderRequests.Update(orderReq);
+                DbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
